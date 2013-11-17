b26143110d540a5198e6b82e575cb34c838deebf
borsi <deadnervster@gmail.com>
2013-11-17 13:54:51 +0100

    frontend fix + css plug in

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <html>
     <head> 
         <link href='http://fonts.googleapis.com/css?family=Just+Me+Again+Down+Here|Great+Vibes|Berkshire+Swash|Sacramento' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="style/style.css" type="text/css" />
     </head>
     <body>
     <div id="header"></div>
 
-    </div>
-
-    <div id="footer"></div>
-
     <div id="content">
+    Dummy szöveg, hosszú úkkal, űkkel és őkkel. Ők sem számítottak igazán erre, ez biztos.
+    <?=$content?>
+    </div>
+    <button>Gomb</button>
+    <div id="footer"></div>
     </body>
 </html>
