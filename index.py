dfccd18664b8a0a30ac083283d5300660e57fac2
sztomi <hello.sztomi@gmail.com>
2013-12-25 20:39:54 +0100

    [add] moved init to be executed only once

--- index.py
+++ index.py
@@ -3,11 +3,11 @@ from wisdom import *
 
 @route('/')
 def wisdom():
-    init()
     return template("index", content=generate())
 
 @route("/assets/<file_name:path>")
 def assets(file_name):
     return bottle.static_file(file_name, root="assets")
 
+init()
 run(host='localhost', port=8080, debug=True)
